5d848a1a1d1d50b3572cc0c81ddf6cea9eb2e806
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-05-30 12:54:05 +0530

    Initial Project Setup

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header({name,title})
+{
+
+  return (
+    <div>
+      <h1>Welcome to Courses Application</h1>
+    </div>
+  );
+}
+
+export default Header;
