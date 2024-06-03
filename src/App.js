0b42e60411e4ac008f9b8ca48fd8b96a75dd7134
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-04 00:59:46 +0530

    divided screen in two parts

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
+import { ToastContainer } from "react-toastify";
 import "./App.css";
 import AddCourse from "./components/AddCourse";
 import AllCourses from "./components/AllCourses";
 import Course from "./components/Course";
 import Home from "./components/Home";
+import Header from "./components/Header"
+import { Col, Container, Row } from "reactstrap";
+import Menus from "./components/Menus";
 
 function App() {
   return (
     <div>
-      <Home />
-      <AllCourses />
-      <AddCourse />
+      <ToastContainer/>
+      <Container>
+
+        <Header></Header>
+        <Row>
+          <Col md={4}>
+           <Menus/>
+          </Col>
+
+          <Col md={8}>
+            <h2>This is content side</h2>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
