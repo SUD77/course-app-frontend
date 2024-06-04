5d04b681965b8c9c4bd11530e0c538e7098560ee
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-05 00:24:00 +0530

    changed menu to make single page application

--- src/components/Menus.js
+++ src/components/Menus.js
@@ -1,25 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import { ListGroup, ListGroupItem } from "reactstrap";
 
 function Menus() {
   return (
     <div>
       <ListGroup>
-        <ListGroupItem tag="a" href="#1">
+        {/* <ListGroupItem tag="a" href="/" action>
           Home
         </ListGroupItem>
-        <ListGroupItem tag="a" href="#1">
+        <ListGroupItem tag="a" href="/add-course" action>
           Add Course
         </ListGroupItem>
-        <ListGroupItem tag="a" href="#1">
+        <ListGroupItem tag="a" href="/view-courses" action>
           View Courses
         </ListGroupItem>
-        <ListGroupItem tag="a" href="#1">
+        <ListGroupItem tag="a" href="#1" action>
           About
         </ListGroupItem>
-        <ListGroupItem tag="a" href="#1">
+        <ListGroupItem tag="a" href="#1" action>
           Contact
-        </ListGroupItem>
+        </ListGroupItem> */}
+
+        <Link tag="a" to="/" action>
+          Home
+        </Link>
+        <Link tag="a" to="/add-course" action>
+          Add Course
+        </Link>
+        <Link tag="a" to="/view-courses" action>
+          View Courses
+        </Link>
+        <Link tag="a" to="#1" action>
+          About
+        </Link>
+        <Link tag="a" to="#1" action>
+          Contact
+        </Link>
       </ListGroup>
     </div>
   );
