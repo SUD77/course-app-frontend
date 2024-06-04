c926366c7ecb3e55c75d8cc88e44b0ed63d499b4
SUD77 <jaiswalsudhanshu20@gmail.com>
2024-06-05 00:23:38 +0530

    imported react router dom

--- src/App.js
+++ src/App.js
@@ -4,27 +4,33 @@ import AddCourse from "./components/AddCourse";
 import AllCourses from "./components/AllCourses";
 import Course from "./components/Course";
 import Home from "./components/Home";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import { Col, Container, Row } from "reactstrap";
 import Menus from "./components/Menus";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <ToastContainer/>
-      <Container>
+      <Router>
+        <ToastContainer />
+        <Container>
+          <Header></Header>
+          <Row>
+            <Col md={4}>
+              <Menus />
+            </Col>
 
-        <Header></Header>
-        <Row>
-          <Col md={4}>
-           <Menus/>
-          </Col>
-
-          <Col md={8}>
-            <h2>This is content side</h2>
-          </Col>
-        </Row>
-      </Container>
+            <Col md={8}>
+              <Routes>
+                <Route path="/" element={<Home />} exact />
+                <Route path="/add-course" element={<AddCourse />} exact />
+                <Route path="/view-courses" element={<AllCourses />} exact />
+              </Routes>
+            </Col>
+          </Row>
+        </Container>
+      </Router>
     </div>
   );
 }
